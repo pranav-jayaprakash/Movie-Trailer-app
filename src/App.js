b4ea940badfac8964c7b8d295443bba2989dad0c
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-08-05 09:01:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios'
 import './App.css';
 
 function App() {
 
 
+  const API_URL = "https://api.themoviedb.org/3"
+  const [movies,setmovies] = useState([])
+
 
   const fetchMovies = async () => {
+
+    const {data:{results}} = await axios.get(`${API_URL}/discover/movie`,{params: {
+      api_key: process.env.REACT_APP_MOVIE_API_KEY
+    }})
+
+    setmovies(results)
 
   }
 
 
   useEffect( ()=>{
-
+    fetchMovies()
   },[])
+
+  const rendermovies = () => {
+    movies.map(movie => {
+      <MovieCard/>
+    })
+  }
+
   return (
     <div className="App">
       <h1>Hello</h1>
+
+      <div className='container'>{rendermovies()}</div>
       
     </div>
   );
---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MovieCard() {
+    
+  return (
+    <div>MovieCard</div>
+  )
+}
+
+export default MovieCard
